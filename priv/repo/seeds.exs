ef661bed76da2453fc55224b9b48b214c7ee9ae5
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-07-22 00:14:13 -0400

    update seeding for users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Schematic.Repo
+alias Schematic.Accounts
+alias Schematic.Accounts.User
+
+# register users
+Repo.delete_all(User)
+Accounts.register_user(%{email: "jr@example.com", password: "Secret123!!!"})
+Accounts.register_user(%{email: "jameson@example.com", password: "Secret123!!!"})
+Accounts.register_user(%{email: "nathan@example.com", password: "Secret123!!!"})
+
+users = Repo.all(User)
+
+IO.puts("Registered #{length(users)} of expected 3 users")

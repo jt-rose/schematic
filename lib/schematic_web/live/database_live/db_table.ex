e6e89ae6a43e9e2ba50f3cd47565ad53c2d4b2d5
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-11-23 20:29:08 -0500

    separate table dimensions calculation

--- lib/schematic_web/live/database_live/db_table.ex
+++ lib/schematic_web/live/database_live/db_table.ex
@@ -3,7 +3,7 @@ defmodule SchematicWeb.DatabaseLive.DbTable do
   use PetalComponents
 
   @grid_tyle_length 3.5
-  @table_width 6
+  # @table_width 6
   @unit "em"
 
   def db_table(assigns) do
@@ -46,15 +46,31 @@ defmodule SchematicWeb.DatabaseLive.DbTable do
   def convert_width_enum("normal"), do: 5
   def convert_width_enum(_), do: 5
 
+  def get_column_boundaries(table, table_width) do
+    column_start = table.grid_column_start
+    column_end = table.grid_column_start + table_width
+
+    {column_start, column_end}
+  end
+
+  def get_row_boundaries(table) do
+    row_start = table.grid_row_start
+    row_end = table.grid_row_start + length(table.table_columns)
+
+    {row_start, row_end}
+  end
+
   def format_table_style(table) do
     table_width = convert_width_enum(table.grid_width)
+    {column_start, column_end} = get_column_boundaries(table, table_width)
+    {row_start, row_end} = get_row_boundaries(table)
 
     "
     width: #{@grid_tyle_length * table_width}#{@unit};
-    grid-column-start: #{table.grid_column_start};
-    grid-column-end: #{table.grid_column_start + table_width};
-    grid-row-start: #{table.grid_row_start};
-    grid-row-end: #{table.grid_row_start + length(table.table_columns)};
+    grid-column-start: #{column_start};
+    grid-column-end: #{column_end};
+    grid-row-start: #{row_start};
+    grid-row-end: #{row_end};
     background: violet;
     z-index: 10;"
   end

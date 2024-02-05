79c04527fdd5d5861380eb3e22445740758489d1
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2024-02-05 00:20:28 -0500

    add default connector svg

--- lib/schematic_web/live/database_live/connector.ex
+++ lib/schematic_web/live/database_live/connector.ex
@@ -18,25 +18,35 @@ defmodule SchematicWeb.DatabaseLive.Connector do
   def connector(assigns) do
     ~H"""
     <%= case @orientation do %>
-      <% "vertical" -> %>
-        <.vertical_line col={@col} row={@row} />
-      <% "horizontal" -> %>
-        <.horizontal_line col={@col} row={@row} />
-      <% "topleft" -> %>
-        <.topleft_angle col={@col} row={@row} />
-      <% "topright" -> %>
-        <.topright_angle col={@col} row={@row} />
-      <% "bottomleft" -> %>
-        <.bottomleft_angle col={@col} row={@row} />
-      <% "bottomright" -> %>
-        <.bottomright_angle col={@col} row={@row} />
+      <% :vertical -> %>
+        <.vertical_line pos={@pos} />
+      <% :horizontal -> %>
+        <.horizontal_line pos={@pos} />
+      <% :top_to_left -> %>
+        <.topleft_angle pos={@pos} />
+      <% :top_to_right -> %>
+        <.topright_angle pos={@pos} />
+      <% :bottom_to_left -> %>
+        <.bottomleft_angle pos={@pos} />
+      <% :bottom_to_right -> %>
+        <.bottomright_angle pos={@pos} /> #TODO: add default
+      <% _ -> %>
+        <.default_node pos={@pos} />
     <% end %>
+    """
+  end
+
+  def default_node(assigns) do
+    ~H"""
+    <svg style={format_angle_style(@pos)} width="3.5em" height="3.5em" viewBox="0 0 10 10">
+      <circle class="stroke-1 stroke-black" cx="5" cy="5" r=".5" />
+    </svg>
     """
   end
 
   def vertical_line(assigns) do
     ~H"""
-    <svg style={format_angle_style(@col, @row)} width="3.5em" height="3.5em" viewBox="0 0 10 10">
+    <svg style={format_angle_style(@pos)} width="3.5em" height="3.5em" viewBox="0 0 10 10">
       <line class="stroke-1 stroke-black" x1="5" x2="5" y1="0" y2="10" />
     </svg>
     """
@@ -44,7 +54,7 @@ defmodule SchematicWeb.DatabaseLive.Connector do
 
   def horizontal_line(assigns) do
     ~H"""
-    <svg style={format_angle_style(@col, @row)} width="3.5em" height="3.5em" viewBox="0 0 10 10">
+    <svg style={format_angle_style(@pos)} width="3.5em" height="3.5em" viewBox="0 0 10 10">
       <line class="stroke-1 stroke-black" x1="0" x2="10" y1="5" y2="5" />
     </svg>
     """
@@ -52,7 +62,7 @@ defmodule SchematicWeb.DatabaseLive.Connector do
 
   def angle(assigns) do
     ~H"""
-    <svg style={format_angle_style(@col, @row)} height="56px" width="56px">
+    <svg style={format_angle_style(@pos)} height="56px" width="56px">
       <polyline points={@points} style="fill:none;stroke:black;stroke-width:6;" />
     </svg>
     """
@@ -60,29 +70,29 @@ defmodule SchematicWeb.DatabaseLive.Connector do
 
   def topleft_angle(assigns) do
     ~H"""
-    <.angle col={@col} row={@row} points="0,28 28,28 28,0" />
+    <.angle pos={@pos} points="0,28 28,28 28,0" />
     """
   end
 
   def topright_angle(assigns) do
     ~H"""
-    <.angle col={@col} row={@row} points="28,0 28,28 56,28" />
+    <.angle pos={@pos} points="28,0 28,28 56,28" />
     """
   end
 
   def bottomleft_angle(assigns) do
     ~H"""
-    <.angle col={@col} row={@row} points="0,28 28,28 28,56" />
+    <.angle pos={@pos} points="0,28 28,28 28,56" />
     """
   end
 
   def bottomright_angle(assigns) do
     ~H"""
-    <.angle col={@col} row={@row} points="56,28 28,28 28,56" />
+    <.angle pos={@pos} points="56,28 28,28 28,56" />
     """
   end
 
-  def format_angle_style(col, row) do
+  def format_angle_style({col, row}) do
     "
     grid-column-start: #{col};
     grid-row-start: #{row};

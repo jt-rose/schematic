940cf9c7b0c3207038dcb764a0d8fa047adc64b2
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2024-02-05 00:24:26 -0500

    define default grid size and border calculation

--- lib/schematic_web/live/database_live/grid_layout.ex
+++ lib/schematic_web/live/database_live/grid_layout.ex
@@ -5,6 +5,13 @@ defmodule SchematicWeb.DatabaseLive.GridLayout do
   @table_width 6
   @unit "em"
 
+  @default_grid %{
+    top_buffer: 1,
+    right_buffer: 101,
+    bottom_buffer: 101,
+    left_buffer: 1
+  }
+
   def grid_layout(assigns) do
     ~H"""
     <div style={@style} class="bg-teal-600 ">
@@ -28,35 +35,18 @@ defmodule SchematicWeb.DatabaseLive.GridLayout do
     "
   end
 
-  # calculate amount of grid rows required to display table
-  def calculate_table_height(table) do
-    std_col_length = table.table_columns |> length
-    enum_col_length = table.enum_columns |> length
-    gen_col_length = table.generated_columns |> length
+  def get_outermost_boundary(boundary, table) do
+    top = min(boundary.top_buffer, table.top_buffer)
+    right = max(boundary.right_buffer, table.right_buffer)
+    bottom = max(boundary.bottom_buffer, table.bottom_buffer)
+    left = min(boundary.left_buffer, table.left_buffer)
 
-    1 + std_col_length + enum_col_length + gen_col_length
-  end
-
-  # calculate width of grid columns to display table
-  def calculate_table_width(_table) do
-    # TODO - update schema
-    3
-  end
-
-  # calculate grid column and grid row start and stop positions
-  def calculate_table_boundaries(_table) do
-    # TODO - avoid using this logic twice
-    %{l: 2, r: 4, t: 0, b: 5}
-  end
-
-  # compare two table boundaries and determine outermost boundaries
-  def get_outermost_boundary(bounds_1, bounds_2) do
-    l = min(bounds_1.l, bounds_2.l)
-    r = max(bounds_1.r, bounds_2.r)
-    t = min(bounds_1.t, bounds_2.t)
-    b = max(bounds_1.b, bounds_2.b)
-
-    %{l: l, r: r, t: t, b: b}
+    %{
+      top_buffer: top,
+      right_buffer: right,
+      bottom_buffer: bottom,
+      left_buffer: left
+    }
   end
 
   # add buffer to table boundaries for readability
@@ -71,28 +61,35 @@ defmodule SchematicWeb.DatabaseLive.GridLayout do
 
   # add additional space around outermost boundaries for adding new tables / readability
   def add_grid_buffer(boundary) do
-    # TODO: either allow negative bounds and recal for css-grid
-    # TODO: or recalc boundary position to renable buffer
-    l = max(boundary.l - 10, 0)
-    r = boundary.r + 10
-    t = max(boundary.t - 10, 0)
-    b = boundary.b + 10
+    %{
+      top_buffer: boundary.top_buffer - 20,
+      right_buffer: boundary.right_buffer + 20,
+      bottom_buffer: boundary.bottom_buffer + 20,
+      left_buffer: boundary.left_buffer - 20
+    }
+  end
 
-    %{l: l, r: r, t: t, b: b}
+  def calculate_boundary_blocked_tiles(
+        %{
+          top_buffer: top,
+          right_buffer: right,
+          bottom_buffer: bottom,
+          left_buffer: left
+        } = _boundary
+      ) do
+    top_border = for c <- left..right, do: {c, top}
+    right_border = for r <- top..bottom, do: {right, r}
+    bottom_border = for c <- left..right, do: {c, bottom}
+    left_border = for r <- top..bottom, do: {left, r}
+
+    top_border ++ right_border ++ bottom_border ++ left_border
   end
 
   # determine amount of grid columns and rows needed
-  def calculate_grid_borders(db) do
-    db.database_tables
-    |> Enum.map(&calculate_table_boundaries/1)
-    |> Enum.reduce(&get_outermost_boundary/2)
-    |> add_table_buffer
+  def calculate_grid_borders(tables) do
+    tables
+    |> Enum.reduce(@default_grid, fn x, acc -> get_outermost_boundary(x, acc) end)
     |> add_grid_buffer
-  end
-
-  def get_negative_space_adjustment(_boundary) do
-    # TODO - keep negative space but adjust all numbers
-    # TODO - on x and y axis of grid to to conform to positive integers
-    # TODO - for grid modeling
+    |> calculate_boundary_blocked_tiles
   end
 end
